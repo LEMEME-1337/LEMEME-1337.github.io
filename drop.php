98101d695702e2642b15d731c6077a5c4948fb5f
LEMEME-1337 <babashola.saleh@challoners.org>
2017-02-04 17:44:34 +0000

    Update drop.php

--- drop.php
+++ drop.php
@@ -1,14 +1,14 @@
-<DOCTYPE html>
-  <html>
-    <body>
-      <?php 
-      $hello_scrubs = $_GET['hello_scrubs'];
-      $russki = $_GET['russki'];
-      $it = $_GET['it'];
-      $happy_new_year = $_GET['happy_new_year'];
+<!DOCTYPE html>
+<html>
+  <body>
+    <?php 
+    $hello_scrubs = $_GET['hello_scrubs'];
+    $russki = $_GET['russki'];
+    $it = $_GET['it'];
+    $happy_new_year = $_GET['happy_new_year'];
 
-      if ($hello_scrubs == TRUE);
-      header("location:https://lememe-1337.github.io/Hello-Scrubs/"); ?>
-    </body>
-  </html>
+    if ($hello_scrubs == TRUE);
+    header("location:https://lememe-1337.github.io/Hello-Scrubs/"); ?>
+  </body>
+</html>
      
